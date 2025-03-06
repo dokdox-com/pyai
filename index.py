d79f1b02c7d5f7a8b0a594bb8ffc890d039046a1
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-03-06 20:06:36 +0900

    __

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import streamlit
 from google import genai
 
-a = "소설을 작성해줘.재미있게 길이는 2000문장 정도"
+a = "소설을 작성해줘.재미있게 길이는 2000문장 정도로"
 
 client = genai.Client(api_key="AIzaSyBx7d8QupJbmbvzyfuCc-QvPmq1cBtAVVU")
 response = client.models.generate_content(
